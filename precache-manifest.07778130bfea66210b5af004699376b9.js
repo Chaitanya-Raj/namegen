f9276e90340b5f49876f0582ac0762f89d93ec54
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2021-07-14 18:14:44 +0530

    deploy

---
+++ precache-manifest.07778130bfea66210b5af004699376b9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59c3dc13345cf9e4fbd",
+    "url": "/namegen/css/app.0fca24a8.css"
+  },
+  {
+    "revision": "af4e1044825ff6b9b24c1203b202c039",
+    "url": "/namegen/fonts/Caveat-VariableFont_wght.af4e1044.ttf"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/namegen/fonts/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/namegen/fonts/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/namegen/fonts/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "031daa195b0aef95c3ef627625ed03a4",
+    "url": "/namegen/index.html"
+  },
+  {
+    "revision": "c59c3dc13345cf9e4fbd",
+    "url": "/namegen/js/app.e342cd1b.js"
+  },
+  {
+    "revision": "20e0d06d2641dc201a53",
+    "url": "/namegen/js/chunk-vendors.38d40c71.js"
+  },
+  {
+    "revision": "57080453fb072ea78f7c0d1f2634fa2c",
+    "url": "/namegen/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/namegen/robots.txt"
+  }
+]);
